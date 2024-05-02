e28cea35366ddf12f5dc7c63a975a8a98f390153
Kim Möller <kim.moller2@protonmail.com>
2024-05-02 16:20:05 +0200

    Add minimal styling to ui

--- admin-ui/src/routes/+page.svelte
+++ admin-ui/src/routes/+page.svelte
@@ -51,37 +51,88 @@
 	}
 </script>
 
-<a href="/"><h1>IAM Admin UI</h1></a>
-<label for="idSearch">Search for identity with id</label>
-<input name="idSearch" />
+<div id="container">
+	<a href="/"><h1>IAM Admin UI</h1></a>
+	<div id="functions">
+		<div id="identitySearch">
+			<label for="idSearch">Search for an identity with Identity ID (WIP)</label>
+			<input name="idSearch" />
+		</div>
 
-<button on:click={() => toggleDialog()}>Create identity</button>
+		<button on:click={() => toggleDialog()}>Create identity</button>
+	</div>
 
-<dialog open={isOpen}>
-	<p>Create identity</p>
-	<label for="firstName">First name</label>
-	<input name="firstName" bind:value={dialogData.firstName} />
-	<label for="lastName">Last name</label>
-	<input name="lastName" bind:value={dialogData.lastName} />
-	<label for="email">Email</label>
-	<input name="email" bind:value={dialogData.email} />
-	<button on:click={() => toggleDialog()}>Cancel</button>
-	<button on:click={() => createIdentity()} type="submit">Create</button>
-</dialog>
+	<dialog open={isOpen}>
+		<p>Create identity</p>
+		<label for="firstName">First name</label>
+		<input name="firstName" bind:value={dialogData.firstName} />
+		<label for="lastName">Last name</label>
+		<input name="lastName" bind:value={dialogData.lastName} />
+		<label for="email">Email</label>
+		<input name="email" bind:value={dialogData.email} />
+		<button on:click={() => toggleDialog()}>Cancel</button>
+		<button on:click={() => createIdentity()} type="submit">Create</button>
+	</dialog>
 
-{#if data !== undefined}
-	<table>
-		<tr>
-			<th>First name</th>
-			<th>Last name</th>
-			<th>Email</th>
-		</tr>
-		{#each data as identity}
+	{#if data !== undefined}
+		<table>
 			<tr>
-				<td><a href="/identity/{identity.id}">{identity.firstName}</a></td>
-				<td>{identity.lastName}</td>
-				<td>{identity.email}</td>
+				<th>First name</th>
+				<th>Last name</th>
+				<th>Email</th>
 			</tr>
-		{/each}
-	</table>
-{/if}
+			{#each data as identity}
+				<tr>
+					<td><a href="/identity/{identity.id}">{identity.firstName}</a></td>
+					<td>{identity.lastName}</td>
+					<td>{identity.email}</td>
+				</tr>
+			{/each}
+		</table>
+	{/if}
+</div>
+
+<style>
+	#container {
+		width: 60%;
+		margin-left: auto;
+		margin-right: auto;
+	}
+
+	#functions {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	#identitySearch {
+		display: flex;
+		flex-direction: column;
+		margin-bottom: 1em;
+	}
+
+	#functions button {
+		height: 3.5em;
+		border-radius: 8px;
+		border: 1px solid #000;
+		background-color: #2e83f2;
+		color: #f1f1f1;
+	}
+
+	table {
+		width: 100%;
+		text-align: left;
+		border: 1px solid;
+		border-collapse: collapse;
+	}
+
+	th,
+	td {
+		padding: 0.3em;
+		border: 1px solid;
+	}
+
+	table tr:nth-child(even) {
+		background-color: #dbdbdb;
+	}
+
+</style>
--- admin-ui/src/routes/identity/[identity]/+page.svelte
+++ admin-ui/src/routes/identity/[identity]/+page.svelte
@@ -50,63 +50,107 @@
 	}
 </script>
 
-<a href="/"><h1>IAM Admin UI</h1></a>
+<div id="container">
+	<a href="/"><h1>IAM Admin UI</h1></a>
 
-{#if identity !== undefined}
-	<div>
-		<h2>{identity.firstName} {identity.lastName}</h2>
-		<h3>{identity.email}</h3>
-	</div>
-	<h2>Accounts</h2>
-	<table>
-		<tr>
-			<th>Username</th>
-			<th>External system</th>
-			<th>Enabled at</th>
-			<th>Disabled at</th>
-			<th>Deleted at</th>
-		</tr>
-		{#if identity.accounts.length > 0}
-			{#each identity.accounts as account}
-				<tr>
-					<td>{account.username}</td>
-					<td>{account.systemId}</td>
-					<td>{account.enabledAt}</td>
-					<td>{account.disabledAt}</td>
-					<td>{account.deletedAt}</td>
-				</tr>
-			{/each}
-		{/if}
-	</table>
-	<h2>Memberships</h2>
-	<button on:click={() => toggleDialog()}>Add membership</button>
+	{#if identity !== undefined}
+		<div>
+			<h2>{identity.firstName} {identity.lastName}</h2>
+			<h3>{identity.email}</h3>
+		</div>
+		<h2>Accounts</h2>
+		<table>
+			<tr>
+				<th>Username</th>
+				<th>External system</th>
+				<th>Enabled at</th>
+				<th>Disabled at</th>
+				<th>Deleted at</th>
+			</tr>
+			{#if identity.accounts.length > 0}
+				{#each identity.accounts as account}
+					<tr>
+						<td>{account.username}</td>
+						<td>{account.systemId}</td>
+						<td>{account.enabledAt}</td>
+						<td>{account.disabledAt}</td>
+						<td>{account.deletedAt}</td>
+					</tr>
+				{/each}
+			{/if}
+		</table>
 
-	<dialog open={isOpen}>
-		<p>Add membership</p>
-		<label for="group">Group</label>
-		<input name="group" bind:value={dialogData.groupId} />
-		<label for="enabledAt">Start date</label>
-		<input name="enabledAt" bind:value={dialogData.enabledAt} />
-		<label for="disabledAt">End date</label>
-		<input name="disabledAt" bind:value={dialogData.disabledAt} />
-		<button on:click={() => toggleDialog()}>Cancel</button>
-		<button on:click={() => createMembership()} type="submit">Create</button>
-	</dialog>
+		<div id="membershipHeader">
+			<h2>Memberships</h2>
+			<button on:click={() => toggleDialog()}>Add membership</button>
+		</div>
 
-	<table>
-		<tr>
-			<th>Group</th>
-			<th>Enabled at</th>
-			<th>Disabled at</th>
-		</tr>
-		{#if identity.memberships.length > 0}
-			{#each identity.memberships as membership}
-				<tr>
-					<td>{membership.group.name}</td>
-					<td>{membership.enabledAt}</td>
-					<td>{membership.disabledAt}</td>
-				</tr>
-			{/each}
-		{/if}
-	</table>
-{/if}
+		<dialog open={isOpen}>
+			<p>Add membership</p>
+			<label for="group">Group</label>
+			<input name="group" bind:value={dialogData.groupId} />
+			<label for="enabledAt">Start date</label>
+			<input name="enabledAt" bind:value={dialogData.enabledAt} />
+			<label for="disabledAt">End date</label>
+			<input name="disabledAt" bind:value={dialogData.disabledAt} />
+			<button on:click={() => toggleDialog()}>Cancel</button>
+			<button on:click={() => createMembership()} type="submit">Create</button>
+		</dialog>
+
+		<table>
+			<tr>
+				<th>Group</th>
+				<th>Enabled at</th>
+				<th>Disabled at</th>
+			</tr>
+			{#if identity.memberships.length > 0}
+				{#each identity.memberships as membership}
+					<tr>
+						<td>{membership.group.name}</td>
+						<td>{membership.enabledAt}</td>
+						<td>{membership.disabledAt}</td>
+					</tr>
+				{/each}
+			{/if}
+		</table>
+	{/if}
+</div>
+
+<style>
+	#container {
+		width: 60%;
+		margin-left: auto;
+		margin-right: auto;
+	}
+
+	#membershipHeader {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	#membershipHeader button {
+		height: 3.5em;
+		border-radius: 8px;
+		border: 1px solid #000;
+		background-color: #2e83f2;
+		color: #f1f1f1;
+		margin-top: 1em;
+	}
+
+	table {
+		width: 100%;
+		text-align: left;
+		border: 1px solid;
+		border-collapse: collapse;
+	}
+
+	th,
+	td {
+		padding: 0.3em;
+		border: 1px solid;
+	}
+
+	table tr:nth-child(even) {
+		background-color: #dbdbdb;
+	}
+</style>
